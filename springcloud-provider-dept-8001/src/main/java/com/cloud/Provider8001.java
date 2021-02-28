0e4daea44c458053ddbf8f23f7b20349d8f1cc30
liuxinjie123 <jackdream@163.com>
2021-02-28 15:24:15 +0800

    eureka server client

--- springcloud-provider-dept-8001/src/main/java/com/cloud/Provider8001.java
+++ springcloud-provider-dept-8001/src/main/java/com/cloud/Provider8001.java
@@ -2,7 +2,12 @@ package com.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * 服务启动后，自动注册到 Eureka中
+ */
+@EnableEurekaClient
 @SpringBootApplication
 public class Provider8001 {
     public static void main(String[] args) {
