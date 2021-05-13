d7a84fbb03e0d758b6fb1b0732594fae02a75197
liuxinjie <jackdream@163.com>
2021-05-14 00:22:35 +0800

    test

--- springcloud-config-client-3355/src/main/java/com/cloud/config/client3355/controller/ConfigController.java
+++ springcloud-config-client-3355/src/main/java/com/cloud/config/client3355/controller/ConfigController.java
@@ -2,12 +2,14 @@ package com.cloud.config.client3355.controller;
 
 import com.cloud.api.common.Response;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping(value = "config")
 @RestController
+@RefreshScope
 public class ConfigController {
 
     @Value("${spring.application.name}")
